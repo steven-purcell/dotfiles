2401cae8b8e076ff4e2e785ae786eda758ef71ce
codisredding <LA6cNv5PehbW>
2016-10-29 15:31:42 -0500

    ignore rethink dir and add git untrack (gut) alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -18,6 +18,7 @@ alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
+alias gut='git update-index --assume-unchanged' # gut config.json will stop tracking config.json
 
 alias gstsh='git stash'
 alias gst='git stash'
